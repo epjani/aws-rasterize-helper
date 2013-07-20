64416dbf269fbc9e56227523688bd5db053b3deb
Edin Pjanic <pjanicedin87@gmail.com>
2013-07-20 16:05:50 +0200

    invoke rasterizations

---
+++ app/controllers/rasterize_controller.rb
@@ -0,0 +1,10 @@
+class RasterizeController < ApplicationController
+	def invoke_rasterization
+		file_name = params[:file_name]
+		csv_values = params[:csv]
+
+		`sudo ./vendor/plugins/phantomjs-linux/bin/phantomjs vendor/plugins/phantomjs-linux/bin/rasterize.js http://http://infograph.doit.ba/parse?csv=#{csv_values} public/infographs/#{file_name}.png`
+
+		render :nothing => true
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ AwsRasterizeHelper::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+
+  get "invoke_rasterization" => "rasterize#invoke_rasterization"
 end
