3b16c2e53ef683495d21062f38c33896c533e1d9
Edin Pjanic <pjanicedin87@gmail.com>
2013-07-20 13:26:13 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwsRasterizeHelper::Application.config.secret_token = '0fa6710b7852b4d9e51ab49e7c79253a3043f364eca3df6b9874018007c23d1f95670c90c2d223708c27ee022e8c6b17c9d3a4eb4a13b549e291bf65736e07b8'
