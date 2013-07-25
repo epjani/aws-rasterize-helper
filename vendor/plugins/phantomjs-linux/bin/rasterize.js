55136a1265dc27a26f293b1b8d64dfa1701c72e2
Edin Pjanic <pjanicedin87@gmail.com>
2013-07-25 16:39:47 +0200

    pdf fix

--- vendor/plugins/phantomjs-linux/bin/rasterize.js
+++ vendor/plugins/phantomjs-linux/bin/rasterize.js
@@ -10,11 +10,11 @@ if (system.args.length < 3 || system.args.length > 5) {
     address = system.args[1];
     output = system.args[2];
     page.viewportSize = { width: 600, height: 600 };
-    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
-        size = system.args[3].split('*');
-        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-                                           : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
-    }
+    // if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
+    //     size = system.args[3].split('*');
+    //     page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
+    //                                        : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
+    // }
     if (system.args.length > 4) {
         page.zoomFactor = system.args[4];
     }
