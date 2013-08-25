3689d0cb07130bf3a2f5fd2405912ffe710f812a
Edin Pjanic <edin.pjanic@atlantbh.com>
2013-08-25 23:07:26 +0200

    language adopt

--- app/controllers/rasterize_controller.rb
+++ app/controllers/rasterize_controller.rb
@@ -2,8 +2,9 @@ class RasterizeController < ApplicationController
 	def invoke_rasterization
 		file_name = params[:file_name]
 		csv_values = params[:csv]
+		locale = params[:locale]
 
-		`sudo ./vendor/plugins/phantomjs-linux/bin/phantomjs vendor/plugins/phantomjs-linux/bin/rasterize.js http://infograph.doit.ba/parse?csv=#{csv_values}&file_name=#{file_name} public/images/infographs/#{file_name}.pdf`
+		`sudo ./vendor/plugins/phantomjs-linux/bin/phantomjs vendor/plugins/phantomjs-linux/bin/rasterize.js http://infograph.doit.ba/parse?csv=#{csv_values}&file_name=#{file_name}&locale=#{locale} public/images/infographs/#{file_name}-#{locale}.png`
 
 		render :nothing => true
 	end
